b49dd255655a63673b4107f00591aa8ab5a906b0
staryu <japenjustnew@gmail.com>
2020-10-18 09:10:39 +0800

    Version1.1.4.2

--- app/src/main/java/com/example/cal/MainActivity.java
+++ app/src/main/java/com/example/cal/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             one, two, three, plus,
             plusorminus, zero, dot, equals;
     TextView display,display1;
-    String numTemp="#",showNumber="";
+    String numTemp="",showNumber="";
     ArrayList<String> numbers = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,181 +78,140 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.clean:
                 numbers.clear();
                 showNumber = "";
-                numTemp = "#";
+                numTemp = "";
                 display.setText(String.valueOf(0));
                 display1.setText("");
                 break;
             case R.id.brackets:
-                
                 break;
             case R.id.quotient:
-                if(!numTemp.equals("#")){
+                if(!numTemp.equals("")){
                     double tmp = Double.parseDouble(numTemp);
-                    tmp = tmp / 100;
-                    numTemp = String.valueOf(tmp);
+                    numbers.add(numTemp);
+                    numTemp = "";
                     showNumber += "%";
-                    showNumber();
                 }
                 break;
             case R.id.division:
-                if (!numTemp.equals("#") ) {
+                if (!numTemp.equals("") ) {
                     numbers.add(numTemp);
                     numTemp = "";
                     showNumber += "÷";
                     numbers.add("÷");
                     numbers.add(",");
                     showNumber();
-                } else if (numbers.size() != 0) {
-                    if (numbers.get(numbers.size() - 1).equals(",")){
-                        numbers.set(numbers.size() - 2, "÷");
-                        showNumber = showNumber.substring(0, showNumber.length() - 1);
-                        showNumber += "÷";
-                        showNumber();
-                    }
-                } else {
-                    display1.setText("請選取數字");
+                } else if (numbers.get(numbers.size() - 1).equals(",")){
+                    numbers.set(numbers.size() - 2, "÷");
+                    showNumber = showNumber.substring(0, showNumber.length() - 1);
+                    showNumber += "÷";
+                    showNumber();
                 }
                 break;
             case R.id.seven:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "7";
                 showNumber += "7";
                 showNumber();
                 break;
             case R.id.eight:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "8";
                 showNumber += "8";
                 showNumber();
                 break;
             case R.id.nine:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "9";
                 showNumber += "9";
                 showNumber();
                 break;
             case R.id.multi:
-                if (!numTemp.equals("#")) {
+                if (!numTemp.equals("") ) {
                     numbers.add(numTemp);
                     numTemp = "";
                     showNumber += "×";
                     numbers.add("×");
                     numbers.add(",");
                     showNumber();
-                } else if (numbers.size() != 0) {
-                    if (numbers.get(numbers.size() - 1).equals(",")){
-                        numbers.set(numbers.size() - 2, "×");
-                        showNumber = showNumber.substring(0, showNumber.length() - 1);
-                        showNumber += "×";
-                        showNumber();
-                    }
-                }  else {
-                    display1.setText("請選取數字");
+                } else if (numbers.get(numbers.size() - 1).equals(",")){
+                    numbers.set(numbers.size() - 2, "×");
+                    showNumber = showNumber.substring(0, showNumber.length() - 1);
+                    showNumber += "×";
+                    showNumber();
                 }
                 break;
             case R.id.four:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "4";
                 showNumber += "4";
                 showNumber();
                 break;
             case R.id.five:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "5";
                 showNumber += "5";
                 showNumber();
                 break;
             case R.id.six:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "6";
                 showNumber += "6";
                 showNumber();
                 break;
             case R.id.sub:
-                if (!numTemp.equals("#") ) {
+                if (!numTemp.equals("") ) {
                     numbers.add(numTemp);
                     numTemp = "";
                     showNumber += "－";
                     numbers.add("－");
                     numbers.add(",");
                     showNumber();
-                } else if (numbers.size() != 0) {
-                    if (numbers.get(numbers.size() - 1).equals(",")) {
-                        numbers.set(numbers.size() - 2, "－");
-                        showNumber = showNumber.substring(0, showNumber.length() - 1);
-                        showNumber += "－";
-                        showNumber();
-                    }
-                } else {
-                    display1.setText("請選取數字");
+                } else if (numbers.get(numbers.size() - 1).equals(",")){
+                    numbers.set(numbers.size() - 2, "－");
+                    showNumber = showNumber.substring(0, showNumber.length() - 1);
+                    showNumber += "－";
+                    showNumber();
                 }
 
                 break;
             case R.id.one:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "1";
                 showNumber += "1";
                 showNumber();
                 break;
             case R.id.two:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "2";
                 showNumber += "2";
                 showNumber();
                 break;
             case R.id.three:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#"))
-                    numTemp = "";
                 numTemp += "3";
                 showNumber += "3";
                 showNumber();
                 break;
             case R.id.plus:
-                if (!numTemp.equals("#") ) {
+                if (!numTemp.equals("") ) {
                     numbers.add(numTemp);
                     numTemp = "";
                     showNumber += "+";
@@ -273,36 +232,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.plusorminus:
                 break;
             case R.id.zero:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#")) {
-                    numTemp = "";
-                }
-                if (!numTemp.equals("#")){
-                    numTemp += "0";
-                    showNumber += "0";
-                    showNumber();
-                }
+                numTemp += "0";
+                showNumber += "0";
+                showNumber();
                 break;
             case R.id.dot:
-                if (numbers.size() != 0){
+                if (numbers.size() != 0)
                     if (numbers.get(numbers.size() - 1).equals(","))
                         numbers.remove(numbers.size() - 1);
-                }
-                if (numTemp.equals("#")) {
-                    numTemp = "";
-                }
-                if (numTemp == "") {
-                    numTemp += "0.";
-                    showNumber += "0.";
-                    showNumber();
-                } else {
-                    numTemp += ".";
-                    showNumber += ".";
-                    showNumber();
-                }
+                numTemp += ".";
+                showNumber += ".";
+                showNumber();
                 break;
             case R.id.equals:
                 if (numTemp.length() != 0) {
@@ -311,10 +254,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 if (numbers.size() != 0){
                     if (!numbers.get(numbers.size() - 1).equals(",")) {
-                        display.setText(calculation(numbers));
+                        calculation();
                         numbers.clear();
                         showNumber = "";
-                        numTemp = "#";
+                        numTemp = "";
                         display1.setText("");
                     } else {
                         display1.setText("請選擇數字");
@@ -324,61 +267,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-    public String calculation (ArrayList num){
-        while(num.contains("×")) {
-            for (int i = 0; i < num.size(); i++) {
-                if (num.get(i).equals("×")) {
-                    num.set(i - 1, operatormulti(num.get(i - 1).toString(), num.get(i + 1).toString()));
-                    display.setText(num.get(i - 1).toString());
-                    num.remove(i + 1);
-                    num.remove(i);
+    public void calculation (){
+        while(numbers.contains("×")) {
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i).equals("×")) {
+                    numbers.set(i - 1, operatormulti(numbers.get(i - 1), numbers.get(i + 1)));
+                    display.setText(numbers.get(i - 1));
+                    numbers.remove(i + 1);
+                    numbers.remove(i);
                 }
             }
         }
         while(numbers.contains("÷")) {
-            for (int i = 0; i < num.size(); i++) {
-                if (num.get(i).equals("÷")) {
-                    num.set(i - 1, operatordiv(num.get(i - 1).toString(), num.get(i + 1).toString()));
-                    display.setText(num.get(i - 1).toString());
-                    num.remove(i + 1);
-                    num.remove(i);
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i).equals("÷")) {
+                    numbers.set(i - 1, operatordiv(numbers.get(i - 1), numbers.get(i + 1)));
+                    display.setText(numbers.get(i - 1));
+                    numbers.remove(i + 1);
+                    numbers.remove(i);
                 }
             }
         }
         while(numbers.contains("+")) {
-            for (int i = 0; i < num.size(); i++) {
-                if (num.get(i).equals("+")) {
-                    num.set(i - 1, operatorPlus(num.get(i - 1).toString(), num.get(i + 1).toString()));
-                    display.setText(num.get(i - 1).toString());
-                    num.remove(i + 1);
-                    num.remove(i);
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i).equals("+")) {
+                    numbers.set(i - 1, operatorPlus(numbers.get(i - 1), numbers.get(i + 1)));
+                    display.setText(numbers.get(i - 1));
+                    numbers.remove(i + 1);
+                    numbers.remove(i);
                 }
             }
         }
         while(numbers.contains("－")) {
-            for (int i = 0; i < num.size(); i++) {
-                if (num.get(i).equals("－")){
-                    num.set(i-1, operatorsub(num.get(i-1).toString(), num.get(i+1).toString()));
-                    display.setText(num.get(i-1).toString());
-                    num.remove(i+1);
-                    num.remove(i);
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i).equals("－")){
+                    numbers.set(i-1, operatorsub(numbers.get(i-1), numbers.get(i+1)));
+                    display.setText(numbers.get(i-1));
+                    numbers.remove(i+1);
+                    numbers.remove(i);
                 }
             }
         }
-        return num.get(0).toString();
+        if(numbers.size() == 1)
+            display.setText(numbers.get(0));
     }
-    public String operatormulti(String i1, String i2){
-        BigDecimal temp, temp1, temp2;
-        BigDecimal[] tp;
+    public String operatorPlus(String i1, String i2){
+        String temp;
+        BigDecimal temp1, temp2;
         temp1 = new BigDecimal(i1);
         temp2 = new BigDecimal(i2);
-        temp = temp1.multiply(temp2);
-        tp = temp.divideAndRemainder(BigDecimal.valueOf(1));
-        if (tp[1].doubleValue() == 0.0){
-            return temp.stripTrailingZeros().toPlainString();
-        } else {
-            return String.valueOf(temp);
-        }
+        temp = (temp1.add(temp2)).toString();
+        return temp;
+    }
+    public String operatorsub(String i1, String i2){
+        String temp;
+        BigDecimal temp1, temp2;
+        temp1 = new BigDecimal(i1);
+        temp2 = new BigDecimal(i2);
+        temp = (temp1.subtract(temp2)).toString();
+        return temp;
+    }
+    public String operatormulti(String i1, String i2){
+        String temp;
+        BigDecimal temp1, temp2;
+        temp1 = new BigDecimal(i1);
+        temp2 = new BigDecimal(i2);
+        temp = (temp1.multiply(temp2)).toString();
+        return temp;
     }
     public String operatordiv(String i1, String i2){
         BigDecimal temp, temp1, temp2;
@@ -388,33 +343,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         temp = temp1.divide(temp2, 10, BigDecimal.ROUND_HALF_UP);
         tp = temp.divideAndRemainder(BigDecimal.valueOf(1));
         if (tp[1].doubleValue() == 0.0){
-            return temp.stripTrailingZeros().toPlainString();
-        } else {
-            return String.valueOf(temp);
-        }
-    }
-    public String operatorPlus(String i1, String i2){
-        BigDecimal temp, temp1, temp2;
-        BigDecimal[] tp;
-        temp1 = new BigDecimal(i1);
-        temp2 = new BigDecimal(i2);
-        temp = temp1.add(temp2);
-        tp = temp.divideAndRemainder(BigDecimal.valueOf(1));
-        if (tp[1].doubleValue() == 0.0){
-            return temp.stripTrailingZeros().toPlainString();
-        } else {
-            return String.valueOf(temp);
-        }
-    }
-    public String operatorsub(String i1, String i2){
-        BigDecimal temp, temp1, temp2;
-        BigDecimal[] tp;
-        temp1 = new BigDecimal(i1);
-        temp2 = new BigDecimal(i2);
-        temp = temp1.subtract(temp2);
-        tp = temp.divideAndRemainder(BigDecimal.valueOf(1));
-        if (tp[1].doubleValue() == 0.0){
-            return temp.stripTrailingZeros().toPlainString();
+            return String.valueOf(temp.intValue());
         } else {
             return String.valueOf(temp);
         }
